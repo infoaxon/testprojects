1ee7bcef81268e3cf56e92e4348571ec3ba35769
Shubham Nagar <shubham.nagar@infoaxon.com>
2020-12-13 23:09:38 +0530

    Learning git so making commit for some tutorial files

---
+++ src/PrintLnClass.java
@@ -0,0 +1,15 @@
+
+public class PrintLnClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hey Good Morning");
+		
+		int number = 89;
+		
+		byte myByte = 127;
+				
+		System.out.println(number);
+		System.out.println(myByte);
+		
+	}
+}
---
+++ src/TutorialTwo.java
@@ -0,0 +1,21 @@
+
+public class TutorialTwo {
+
+	public static void main(String[] args) {
+		
+		String mystring = "Shubham";
+		
+		System.out.println(mystring);
+		
+		int value = 0;
+		
+		while (value < 10) {
+			
+			System.out.println(value);
+			value = value + 1;
+		}
+		
+	
+	}
+
+}
